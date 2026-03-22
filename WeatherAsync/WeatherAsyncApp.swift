731e9ff40503068cc92275599ecf856dc4c7e9d5
wannabemountainking <kdy0096@gmail.com>
2026-03-22 14:35:33 +0900

    Initial Commit

---
+++ WeatherAsync/WeatherAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAsyncApp.swift
+//  WeatherAsync
+//
+//  Created by YoonieMac on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
