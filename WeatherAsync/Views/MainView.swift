892ef2bc74d74bb0d7ef3289e2640e899b27e99c
wannabemountainking <kdy0096@gmail.com>
2026-03-24 12:52:50 +0900

    finished

--- WeatherAsync/Views/MainView.swift
+++ WeatherAsync/Views/MainView.swift
@@ -11,27 +11,29 @@ struct MainView: View {
     @State private var vm: WeatherViewModel = .init()
     @State private var currentCity: String = "서울"
     @State private var cityName: String = "서울"
+    var cityWeathers: [Weather] {
+        vm.weathers.filter { $0.cityName != cityName }
+    }
     
     var body: some View {
         NavigationStack {
             VStack(spacing: 20) {
-                Section {
-                    HStack(spacing: 10) {
-                        Button("", systemImage: "magnifyingglass") {
-                            Task {
-                                vm.currentWeatherData = await vm.fetchWeatherData(cityName: currentCity)
-                                cityName = currentCity
-                            }
+                HStack(spacing: 10) {
+                    Button("", systemImage: "magnifyingglass") {
+                        Task {
+                            vm.currentWeatherData = await vm.fetchWeatherData(cityName: currentCity)
+                            cityName = currentCity
                         }
-                        .font(.title)
-                        TextField("도시 검색", text: $currentCity)
-                            .textFieldStyle(.roundedBorder)
-                            .font(.title2)
-                            .autocorrectionDisabled()
-                            .autocapitalization(.none)
                     }
-                    .padding(20)
+                    .font(.title)
+                    TextField("도시 검색", text: $currentCity)
+                        .textFieldStyle(.roundedBorder)
+                        .font(.title2)
+                        .autocorrectionDisabled()
+                        .autocapitalization(.none)
                 }
+                .padding(.top, 20)
+                .padding(10)
                 
                 if vm.isLoading {
                     ProgressView("불러오는 중 ...")
@@ -39,6 +41,27 @@ struct MainView: View {
                 } else if let weatherInfo = vm.currentWeatherData {
 
                     WeatherDetailView(weatherInfo: weatherInfo, cityName: cityName)
+                    
+                    ForEach(cityWeathers, id: \.id) { weather in
+                        NavigationLink {
+                            WeatherDetailView(weatherInfo: weather, cityName: weather.cityName)
+                        } label: {
+                            HStack {
+                                Text(weather.cityName)
+                                    .font(.title2)
+                                    .padding(.horizontal)
+                                Spacer()
+                                Text("\(weather.currentTemp) ℃")                            .font(.title2)
+                                    .fontWeight(.light)
+                                    .padding(.horizontal)
+                            }
+                        }
+                        .frame(height: 30)
+                        .frame(maxWidth: .infinity)
+                        .padding(5)
+                        .padding(.horizontal)
+                        .buttonStyle(.borderedProminent)
+                    }
                 } else {
                     ContentUnavailableView("네트워크 오류", systemImage: "exclamationmark.triangle", description: Text("날씨를 불러 올 수 없어요").font(.title3))
                     Button(action: {
@@ -54,9 +77,11 @@ struct MainView: View {
                     .buttonStyle(.borderedProminent)
                     .padding()
                 }
+                
                 Spacer()
             } //:VSTACK
             .navigationTitle("🌦️ 날씨")
+            .navigationBarTitleDisplayMode(.large)
         } //:NAVSTACK
     }
 }
--- WeatherAsync/Views/WeatherDetailView.swift
+++ WeatherAsync/Views/WeatherDetailView.swift
@@ -9,24 +9,24 @@ import SwiftUI
 
 
 struct WeatherDetailView: View {
-    @State private var vm: WeatherViewModel = .init()
     var weatherInfo: Weather?
     var cityName: String?
-    var cityWeathers: [Weather] {
-        vm.weathers.filter { $0.cityName != self.cityName }
-    }
-    
+
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading, spacing: 10) {
             
             ZStack {
                 RoundedRectangle(cornerRadius: 16)
-                    .background(Color(uiColor: UIColor(red: 28.0, green: 109.0, blue: 208.0, alpha: 1.0)))
+                    .fill(Color(hex: "1C6DD0") ?? Color.gray.opacity(0.5))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 180)
                     .padding(16)
                 
                 VStack(alignment: .leading, spacing: 10) {
                     Text(cityName ?? "장소가 목록에 없습니다")
+                        .font(.title2.bold())
                     Text("\(weatherInfo?.currentTemp ?? "0.0") ℃")
+                        .font(Font.system(size: 52, weight: .thin))
                     Text("최고  \(weatherInfo?.highestTemp ?? "0.0") ℃ / 최저  \(weatherInfo?.lowestTemp ?? "0.0") ℃")
                 }
                 .foregroundStyle(.white)
@@ -34,48 +34,67 @@ struct WeatherDetailView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 16))
                 .padding(16)
             }
-            
-            
-            
     
-            HStack {
-                VStack(spacing: 10) {
-                    Text("습도")
-                    Text("\(weatherInfo?.currentHumidity ?? "0.0") %")
-                }
-                .frame(maxWidth: .infinity)
-                VStack(spacing: 10) {
-                    Text("풍속")
-                    Text("\(weatherInfo?.windSpeed ?? "0.0") km/h")
-                }
-                .frame(maxWidth: .infinity)
-                VStack(spacing: 10) {
-                    Text("체감온도")
-                    Text("\(weatherInfo?.currentHumidity ?? "0.0") ℃")
-                }
-                .frame(maxWidth: .infinity)
-            }
-            .padding(.horizontal, 30)
-            
-            List {
-                ForEach(self.cityWeathers, id: \.id) { weather in
-                    HStack {
-                        Text(weather.cityName)
-                        Spacer()
-                        Text(weather.currentTemp)
+            HStack(spacing: 20) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color(uiColor: UIColor.secondarySystemBackground))
+                    VStack(spacing: 10) {
+                        Text("습도")
+                        Text("\(weatherInfo?.currentHumidity ?? "0.0") %")
                     }
+                    .frame(maxWidth: .infinity)
                 }
+                .frame(height: 80)
+                .frame(maxWidth: .infinity)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color(uiColor: UIColor.secondarySystemBackground))
+                    VStack(spacing: 10) {
+                        Text("풍속")
+                        Text("\(weatherInfo?.windSpeed ?? "0.0") km/h")
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .frame(height: 80)
+                .frame(maxWidth: .infinity)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color(uiColor: UIColor.secondarySystemBackground))
+                    VStack(spacing: 10) {
+                        Text("체감온도")
+                        Text("\(weatherInfo?.apparentTemp ?? "0.0") ℃")
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .frame(height: 80)
+                .frame(maxWidth: .infinity)
             }
-            
+            .padding(.horizontal, 20)
+                
             Spacer()
         }
     }
 }
 
 extension Color {
-    init(hex: String) {
-        var hexArr: [String] = []
+    init?(hex: String) {
+        var rgb: [CGFloat] = []
         
+        stride(from: 0, to: hex.count as Int, by: 2).forEach { index in
+            let start = hex.index(hex.startIndex, offsetBy: index)
+            let end = hex.index(start, offsetBy: 2)
+            let colorString = String(hex[start..<end])
+            guard let colorInt = Int(colorString, radix: 16) else { return }
+            let colorCGFloat = CGFloat(colorInt) / 255
+            rgb.append(colorCGFloat)
+        }
+        if rgb.count != 3 {
+            return nil
+        }
+        self = Color(uiColor: UIColor(red: rgb[0], green: rgb[1], blue: rgb[2], alpha: 1.0))
     }
 }
 
